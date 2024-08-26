36bd8089c434f424db4aeb3df868f690f0b5bae3
BARK Protocol <dev@barkprotocol.net>
2024-08-26 07:29:27 +0000

    fix error: Invalid <Link> with <a> child.

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,14 +1,14 @@
 import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "Solana Actions and Blinks",
-  description: "",
-  url: "https://",
-  ogImage: "https://tx.shadcn.com/og.jpg",
+  name: "BARK Blink",
+  description: "Solana Actions and Blinks",
+  url: "https://blink.barkprotocol.net",
+  ogImage: "https://ucarecdn.com/4bc61874-018e-481b-bc07-c3f93956f7bd/tchest.png",
+  logoUrl: "https://ucarecdn.com/b2ad5d9c-abe8-4295-9f2b-274835546617/blinklight.png",
   links: {
-    twitter: "https://twitter.com/nickfrosty",
-    github:
-      "https://github.com/solana-developers/solana-actions/tree/main/examples/next-js",
+    twitter: "https://twitter.com/bark_protocol",
+    github: "https://github.com/barkprotocol/blink",
     docs: "https://solana.com/docs/advanced/actions",
   },
 };
